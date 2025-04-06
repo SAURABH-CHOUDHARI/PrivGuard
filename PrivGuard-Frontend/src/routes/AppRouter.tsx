3bc747bfa838aa693ce3d5c351422b9e24611c9f
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-06 19:48:23 +0530

    AppRouter made prototype

---
+++ PrivGuard-Frontend/src/routes/AppRouter.tsx
@@ -0,0 +1,25 @@
+import PrivateRoute from "@/components/PrivateRoute";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeroPage from "@/pages/Hero";
+import AuthPage from "@/pages/AuthPage";
+import PasswordVault from "@/pages/PasswordVault";
+import PasswordDetail from "@/pages/PasswordDetail";
+import AddPassword from "@/pages/AddPassword";
+
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<AuthPage />} />
+                <Route path="/" element={<HeroPage />} />
+                <Route path="/dashboard" element={<PrivateRoute><AuthPage /></PrivateRoute>} />
+                <Route path="/vault" element={<PrivateRoute><PasswordVault /></PrivateRoute>} />
+                <Route path="/vault/add-password" element={<PrivateRoute><AddPassword /></PrivateRoute>} />
+                <Route path="/password/:id" element={<PasswordDetail />} /> {/* Passkey Protected */}
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
