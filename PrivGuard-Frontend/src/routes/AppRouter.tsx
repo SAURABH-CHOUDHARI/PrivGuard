0456148a0952b8ff85e7368e7207a4fe0a9ce8a0
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-11 12:51:53 +0530

    new route - check Password

--- PrivGuard-Frontend/src/routes/AppRouter.tsx
+++ PrivGuard-Frontend/src/routes/AppRouter.tsx
@@ -6,6 +6,7 @@ import AddPassword from "@/pages/AddPassword";
 import Dashboard from "@/pages/Dashboard";
 import HomePage from "@/sections/HomePage";
 import FakeIdentity from "@/pages/FakeIdentity";
+import CheckPassword from "@/pages/CheckPassword";
 
 
 const AppRouter = () => {
@@ -17,7 +18,8 @@ const AppRouter = () => {
                 <Route path="/vault" element={<PrivateRoute><PasswordVault /></PrivateRoute>} />
                 <Route path="/add-website" element={<PrivateRoute><AddPassword /></PrivateRoute>} />
                 <Route path="/password/:id" element={<PasswordDetail />} /> {/* Passkey Protected */}
-                <Route path="/identity" element={<FakeIdentity />} />
+                <Route path="/identity" element={<PrivateRoute><FakeIdentity /></PrivateRoute>} />
+                <Route path="/checkpassword" element={<CheckPassword />} />
             </Routes>
         </Router>
     );
