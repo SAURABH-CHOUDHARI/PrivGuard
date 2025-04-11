bf1ab78b77d5ceb0a7659faa34743f011a253ef0
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-11 13:05:12 +0530

    check password page v2

--- PrivGuard-Frontend/src/pages/CheckPassword.tsx
+++ PrivGuard-Frontend/src/pages/CheckPassword.tsx
@@ -3,9 +3,9 @@ import Navbar from "@/components/Navbar";
 
 const CheckPassword = () => {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
-            <div className="min-h-screen flex flex-col items-center justify-center px-4">
+            <div className="flex-grow flex py-12 justify-center px-4">
                 <div className="max-w-xl w-full">
                     <h1 className="text-2xl font-bold text-center mb-6">
                         🔐 Check if your password is safe
@@ -13,7 +13,7 @@ const CheckPassword = () => {
                     <PasswordBreachCheck />
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
