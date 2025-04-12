7aafca06e2bd15be72afb5b92b7da36b8a01f677
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-12 13:52:06 +0530

    migration and uuid setup for alpine postgress

---
+++ PrivGuard-Backend/db/migrations/migrate.go
@@ -0,0 +1,23 @@
+package migrations
+
+import (
+	"log"
+
+	"gorm.io/gorm"
+
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/internal/models"
+)
+
+func AutoMigrate(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&models.User{},
+		&models.Vault{},
+		&models.Service{},
+	)
+
+	if err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
+
+	log.Println("✅ Database migrated successfully")
+}
