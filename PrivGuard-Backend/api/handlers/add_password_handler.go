64702f530c72a9d4e727c4ffad9d9c072c0c112a
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-12 14:01:36 +0530

    add password_handler

---
+++ PrivGuard-Backend/api/handlers/add_password_handler.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/internal/services"
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/pkg/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+type AddPasswordRequest struct {
+	ServiceName string `json:"service"`
+	Domain      string `json:"domain"`
+	Logo        string `json:"logo"`
+	Password    string `json:"password"`
+	Notes       string `json:"notes"`
+}
+
+func AddPasswordHandler(repo storage.Repository) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		// Get userID from context (assuming you set it in middleware)
+		userIDInterface := c.Locals("user_id")
+		userID, ok := userIDInterface.(string)
+		if !ok {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "User ID not available in context",
+			})
+		}
+		
+
+		var req AddPasswordRequest
+		if err := c.BodyParser(&req); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request"})
+		}
+
+		err := services.AddPasswordToVault(repo.DB, userID, req.ServiceName, req.Domain, req.Logo, req.Password, req.Notes)
+
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to save password"})
+		}
+
+		return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "Password saved successfully"})
+	}
+}
