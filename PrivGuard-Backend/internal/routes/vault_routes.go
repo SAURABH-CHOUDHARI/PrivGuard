f3896792c5b34f4cb0ada0220785c6686332f523
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-16 18:01:44 +0530

    vault routes are added

---
+++ PrivGuard-Backend/internal/routes/vault_routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/internal/middleware"
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/api/handlers"
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/pkg/storage"
+)
+
+func RegisterRoutes(router fiber.Router, repo storage.Repository,  ) {
+	protected := router.Group("/protected", middleware.AuthMiddleware(repo))
+    
+    // Vault routes
+    vault := protected.Group("/vault")
+    vault.Get("/", handlers.VaultHandler(repo))
+    vault.Post("/add", handlers.AddPasswordHandler(repo))
+    vault.Get("/:id", handlers.GetPasswordDetailHandler(repo))	
+}
