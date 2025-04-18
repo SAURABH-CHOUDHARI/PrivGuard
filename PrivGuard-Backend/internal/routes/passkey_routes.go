50bf754282610b279e740a47b2ad5df3ee7a9b9d
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-19 01:14:53 +0530

    implemented user rate limit for passkey routes

--- PrivGuard-Backend/internal/routes/passkey_routes.go
+++ PrivGuard-Backend/internal/routes/passkey_routes.go
@@ -1,21 +1,33 @@
 package routes
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"github.com/go-webauthn/webauthn/webauthn"
-	
+	"github.com/gofiber/fiber/v2"
+	"time"
+
 	"github.com/SAURABH-CHOUDHARI/privguard-backend/api/handlers"
-	"github.com/SAURABH-CHOUDHARI/privguard-backend/pkg/storage"
 	"github.com/SAURABH-CHOUDHARI/privguard-backend/internal/middleware"
+	"github.com/SAURABH-CHOUDHARI/privguard-backend/pkg/storage"
 )
 
-func RegisterPasskeyRoutes(router fiber.Router, repo storage.Repository,  wa *webauthn.WebAuthn) {
+func RegisterPasskeyRoutes(router fiber.Router, repo storage.Repository, wa *webauthn.WebAuthn) {
 
 	auth := router.Group("/auth", middleware.AuthMiddleware(repo))
 
-	auth.Post("/register/start", handlers.StartRegistrationHandler(repo, wa))
-	auth.Post("/register/finish", handlers.FinishRegistrationHandler(repo, wa))
-	auth.Post("/login/start", handlers.StartLoginHandler(repo, wa))
-	auth.Post("/login/finish", handlers.FinishLoginHandler(repo, wa))
+	auth.Post("/register/start",
+		middleware.UserRateLimit(repo, 20, 1*time.Minute, "register_start"),
+		handlers.StartRegistrationHandler(repo, wa))
+
+	auth.Post("/register/finish",
+		middleware.UserRateLimit(repo, 20, 1*time.Minute, "register_finish"),
+		handlers.FinishRegistrationHandler(repo, wa))
+
+	auth.Post("/login/start",
+		middleware.UserRateLimit(repo, 100, 1*time.Minute, "login_start"),
+		handlers.StartLoginHandler(repo, wa))
+
+	auth.Post("/login/finish",
+		middleware.UserRateLimit(repo, 100, 1*time.Minute, "login_finish"),
+		handlers.FinishLoginHandler(repo, wa))
 
 }
