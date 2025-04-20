785ac7c9eb3bd20444b14fe8c3d0f313c14da47f
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-20 14:56:44 +0530

    TOTP Provied wrapped around app

--- PrivGuard-Frontend/src/main.tsx
+++ PrivGuard-Frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeProvider";
 import { ClerkProvider } from '@clerk/clerk-react'
+import { TOTPProvider } from "./context/TOTPContext.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -15,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
-        <App />
+        <TOTPProvider>
+          <App />
+        </TOTPProvider>
       </ClerkProvider>
     </ThemeProvider>
   </React.StrictMode>
