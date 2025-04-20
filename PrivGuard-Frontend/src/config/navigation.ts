b5630237c442681c89b33798597edd544045a3c1
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-20 14:56:15 +0530

    new NavItem Added

--- PrivGuard-Frontend/src/config/navigation.ts
+++ PrivGuard-Frontend/src/config/navigation.ts
@@ -5,6 +5,7 @@ import {
     ShieldAlert,
     UserSquare2,
     ShieldCheck,
+    Lock
 } from "lucide-react";
 
 export const navLinks = [
@@ -14,4 +15,5 @@ export const navLinks = [
     { name: "Check Breaches", path: "/check-breaches", icon: ShieldAlert },
     { name: "Fake Identity", path: "/identity", icon: UserSquare2 },
     { name: "Password Check", path: "/checkpassword", icon: ShieldCheck },
+    { name: "Security Center", path: "/security", icon: Lock },
 ];
