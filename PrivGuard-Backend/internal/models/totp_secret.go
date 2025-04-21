fde2e48b10f41da2048c94c4d3f795ecca44353a
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-04-21 22:09:03 +0530

    TOTP model updated for isConfiremed for setup

--- PrivGuard-Backend/internal/models/totp_secret.go
+++ PrivGuard-Backend/internal/models/totp_secret.go
@@ -5,9 +5,12 @@ import(
 )
 
 
+// internal/models/totp_secret.go
+
 type TOTPSecret struct {
-    UserID   string `gorm:"primaryKey"`
-    Secret   string // base32‑encoded
-    Enabled  bool
-    CreatedAt time.Time
+    UserID      string    `gorm:"primaryKey"`
+    Secret      string    // base32‑encoded
+    Enabled     bool      // “enforced” flag, can stay false for now
+    IsConfirmed bool      // ← New: true only after a successful Verify
+    CreatedAt   time.Time
 }
